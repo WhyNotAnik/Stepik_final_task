0c8d46c929818eb6dc4074c51ceb5e31c244a36f
Anik <AnikWhyNot@gmail.com>
2021-05-12 00:32:08 +0300

    only spelling error corrected

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,7 +12,7 @@ class ProductPage(BasePage):
         message_this_product_added_to_cart = self.browser.find_element(*ProductPageLocators.MESSAGE_THIS_PRODUCT_ADDED_TO_CART).text
         assert product_name == message_this_product_added_to_cart, "The item added to the cart is different from the intended"
 
-    def should_be_message_of_cart_price_equal_to_product_prise(self):
+    def should_be_message_of_cart_price_equal_to_product_priсe(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         message_of_cart_price_equal_to_product_price = self.browser.find_element(*ProductPageLocators.MESSAGE_OF_CART_PRICE_EQUAL_TO_PRODUCT_PRICE).text
         assert product_price == message_of_cart_price_equal_to_product_price, "The price of the cart is not equal to product's price"
